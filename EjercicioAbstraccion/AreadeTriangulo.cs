dcfbb963fb15e54c2368af7b47b397e41309b54a
OAppenzeller <o.appenzeller@yahoo.com>
2024-10-27 15:03:41 -0600

    Create AreadeTriangulo.cs
    I made a class and used the mathematical formula to calculate the area of a triangle

---
+++ EjercicioAbstraccion/AreadeTriangulo.cs
@@ -0,0 +1,26 @@
+﻿namespace EjercicioAbstraccion
+{
+    class AreadeTriangulo : FormulaMatematica
+    {
+        private double baseTriangulo;
+        private double alturaTriangulo;
+        private double area;
+
+        public void AreaTriangulo(double baseTriangulo, double alturaTriangulo)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.alturaTriangulo = alturaTriangulo;
+        }
+
+        public override void Calcular()
+        {
+            area = (baseTriangulo * alturaTriangulo) / 2;
+           
+        }
+
+        public override void Imprimir()
+        {
+            Console.WriteLine("El area del triangulo es: " + area);
+        }
+    }
+}
